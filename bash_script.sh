09829e9e26b987b0ec20ecb7cf26c467b4ce018e
llffssaa <lf.agostinho33@gmail.com>
2024-10-14 21:04:06 -0300

    Adicionando o script bash e o arquivo .txt no repositório da P1

---
+++ bash_script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Mostrar a pasta/diretório atual
+pwd
+
+#Gerar uma lista com o conteúdo da pasta home em detalhes, e direcioná-los para um arquivo .txt
+ls -la $HOME > lista_HOME.txt
+
+#Imprimir a data atual na tela
+date
+
+
